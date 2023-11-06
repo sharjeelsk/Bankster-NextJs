262b4ee2728c4dcfde93af6effb27f77195ede6b
Faisal <iamfaisalkhaan@gmail.com>
2023-11-06 23:42:52 +0530

    changes by faisal

--- src/app/layout.js
+++ src/app/layout.js
@@ -54,26 +54,40 @@ export default function RootLayout({ children }) {
       <head>
         <link
           rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
+          rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          // integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-          // crossorigin="anonymous"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossOrigin="anonymous"
         />
 
         <Script
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossorigin="anonymous"
+        ></Script>
+        <Script
           src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
           integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-          // crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></Script>
         <Script
           src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
           integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-          // crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></Script>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
         />
+        <Script
+          src="https://kit.fontawesome.com/9e69520da3.js"
+          crossOrigin="anonymous"
+        ></Script>
         <Script src="semantic/dist/semantic.min.js"></Script>
+        <Script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></Script>
       </head>
       <body>
         <Providers>
